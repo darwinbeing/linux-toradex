foundriesio/linux

pcie-imx6: improve reset function for reseting EP
If the reset gpio for a PCIe switch is invalid, but a gpio for
reseting downstream EP devices is valid no reset pulse for the EP devices is
generated.
(i.e. in the device tree reset-gpio is not defined but reset-ep-gpio is defined)

The patch fixes this.

Original patch from Juha Kokkonen, Huoltamoeletroniikka Ky

drivers/pci/host/pci-imx6.c
@@ -290,6 +290,10 @@ static int imx6_pcie_deassert_core_reset(struct pcie_port *pp)
 		msleep(1);
 		if (gpio_is_valid(imx6_pcie->reset_ep_gpio))
 			gpio_set_value(imx6_pcie->reset_ep_gpio, 0);
+	} else if (gpio_is_valid(imx6_pcie->reset_ep_gpio)) {
+		gpio_set_value(imx6_pcie->reset_ep_gpio, 1);
+		msleep(100);
+		gpio_set_value(imx6_pcie->reset_ep_gpio, 0);
 	}
 	return 0;
 

darwinbeing/linux-toradex

(cherry picked from commit f593ebc9921013a992c1d096bf4cbad17c1af01b)
Conflicts:
	drivers/pci/host/pci-imx6.c

drivers/pci/host/pci-imx6.c
@@ -435,8 +435,12 @@ static int imx6_pcie_deassert_core_reset(struct pcie_port *pp)
 		if (gpio_is_valid(imx6_pcie->reset_ep_gpio))
 			gpio_set_value_cansleep(imx6_pcie->reset_ep_gpio, 0);
 		mdelay(20);
+	} else if (gpio_is_valid(imx6_pcie->reset_ep_gpio)) {
+		gpio_set_value_cansleep(imx6_pcie->reset_ep_gpio, 1);
+		mdelay(100);
+		gpio_set_value_cansleep(imx6_pcie->reset_ep_gpio, 0);
+		mdelay(20);
 	}
-
 	/*
 	 * Release the PCIe PHY reset here
 	 */